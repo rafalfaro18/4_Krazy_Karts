dfa5b06522a93feaf7363be8acb01a68511b221f
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-10-10 23:34:34 -0600

    Simulates Unacknowledged Moves (server replays client moves)

--- KrazyKarts/Source/KrazyKarts/GoKart.h
+++ KrazyKarts/Source/KrazyKarts/GoKart.h
@@ -59,7 +59,7 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
-	void SimulateMove(FGoKartMove Move);
+	void SimulateMove(const FGoKartMove& Move);
 
 	FGoKartMove CreateMove(float DeltaTime);
 	void ClearAcknowledgeMoves(FGoKartMove LastMove);
