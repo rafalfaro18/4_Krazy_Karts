0c5f90007300b31293a3afbb342c65d8ed0d8cc9
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-10-11 21:56:25 -0600

    Adds new actor comp GoKartMovementComponent

---
+++ KrazyKarts/Source/KrazyKarts/GoKartMovementComponent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GoKartMovementComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class KRAZYKARTS_API UGoKartMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGoKartMovementComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
