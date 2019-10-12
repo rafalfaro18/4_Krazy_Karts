8810c3f33c87e0597a4bbfd1ba87160c6b7c33f9
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-10-11 22:16:22 -0600

    Ident

--- KrazyKarts/Source/KrazyKarts/GoKartMovementComponent.h
+++ KrazyKarts/Source/KrazyKarts/GoKartMovementComponent.h
@@ -60,23 +60,23 @@ private:
 
 	// The mass of the car (kg).
 	UPROPERTY(EditAnywhere)
-		float Mass = 1000;
+	float Mass = 1000;
 
 	// The force applied to the car when the throttle is fully down (N).
 	UPROPERTY(EditAnywhere)
-		float MaxDrivingForce = 10000;
+	float MaxDrivingForce = 10000;
 
 	// Minimum radius of the car turning circle at full lock (m).
 	UPROPERTY(EditAnywhere)
-		float MinTurningRadius = 10;
+	float MinTurningRadius = 10;
 
 	// Higher means more drag.
 	UPROPERTY(EditAnywhere)
-		float DragCoefficient = 16;
+	float DragCoefficient = 16;
 
 	// Higher means more rolling resistance.
 	UPROPERTY(EditAnywhere)
-		float RollingResistanceCoefficient = 0.015;
+	float RollingResistanceCoefficient = 0.015;
 
 	FVector Velocity;
 
