12ee945b5e4e351e9f52349ed8c8f03e06955fcc
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-10-11 22:26:54 -0600

    Fix pointer to incomplete class type errors

--- KrazyKarts/Source/KrazyKarts/GoKartMovementComponent.cpp
+++ KrazyKarts/Source/KrazyKarts/GoKartMovementComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "GoKartMovementComponent.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 // Sets default values for this component's properties
 UGoKartMovementComponent::UGoKartMovementComponent()
